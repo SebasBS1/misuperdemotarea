cacd68b111f977ad9c3058fdc6159a431bd4426a
SebasBS1 <A00227588@tec.mx>
2025-11-11 15:55:19 -0700

    Fix deletePost to use post ID from request params

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -31,12 +31,14 @@ export const getAllPosts = async (req, res) => {
 };
 
 export const deletePost = async (req, res) => {
+    const {id} = req.params;
+    
     try{
         const deletePostQuery = `
             DELETE FROM posts
             WHERE id = $1
         `;
-        const result = await query(deletePostQuery);
+        const result = await query(deletePostQuery, [id]);
         res.json(result.rows[0]);
     } catch (error) {
         res.status(400).json({error: error.message});
